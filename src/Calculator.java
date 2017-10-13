3d2db82e23ed045c39876f10b1bf0138dc40a84c
Dayton Shor <dayton.shor@gmain.com>
2017-10-13 09:53:07 -0400

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,49 @@
+
+//( 2 * 32 ) - 23 + 6 / 3 + 12 % 2
+
+public class Calculator {
+    public static void main(String[] args){
+        
+        String[] s = new String [15];
+        s[0] = "(";
+        s[1] = "2";
+        s[2] = "*";
+        s[3] = "32";
+        s[4] = ")";
+        s[5] = "-";
+        s[6] = "23";
+        s[7] = "+";
+        s[8] = "6";
+        s[9] = "/";
+        s[10] = "3";
+        s[11] = "+";
+        s[12] = "12";
+        s[13] = "%";
+        s[14] = "2";
+
+    }
+
+    public static double add(double a, double b){
+        return 0;
+    }
+
+    public static double subtract(double a, double b){
+        return 0;
+    }
+
+    public static double multiply(double a, double b){
+        return 0;
+    }
+
+    public static double divide(double a, double b){
+        return 0;
+    }
+
+    public static double modulo(double a, double b){
+        return 0;
+    }
+
+    public static double percent(double a, double b){
+        return 0;
+    }
+}
---
+++ src/IllegalOperationException.java
@@ -0,0 +1,3 @@
+public class IllegalOperationException extends IllegalArgumentException {
+
+}
